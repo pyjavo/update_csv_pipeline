b93a20de4d40212753cd6ef3c642b412469ab1d6
Javier Daza <javierdazacomas@gmail.com>
2023-06-21 22:42:46 -0500

    VARCHAR and shopify_id NOT NULL

--- airflow/dags/insert_existing_csv_data.sql
+++ airflow/dags/insert_existing_csv_data.sql
@@ -3,6 +3,4 @@ INFILE '/var/lib/mysql-files/archivo.csv'
 INTO TABLE raw_data 
 FIELDS TERMINATED BY ',' 
 LINES TERMINATED BY '\n'
-IGNORE 1 ROWS
-(@preferred_delivery_date)
-SET preferred_delivery_date = NULLIF(@preferred_delivery_date,'');
+IGNORE 1 ROWS;
