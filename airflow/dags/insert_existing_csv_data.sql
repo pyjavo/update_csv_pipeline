0beefee363f9fe9c9a399f649a80d7a81e27d1bd
Javier Daza <javierdazacomas@gmail.com>
2023-06-21 22:34:27 -0500

    NULLIF in insert data sql file

--- airflow/dags/insert_existing_csv_data.sql
+++ airflow/dags/insert_existing_csv_data.sql
@@ -4,3 +4,5 @@ INTO TABLE raw_data
 FIELDS TERMINATED BY ',' 
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS
+(@preferred_delivery_date)
+SET preferred_delivery_date = NULLIF(@preferred_delivery_date,'');
